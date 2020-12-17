a9bd5bd22499152c77b7cac8c9c83ececc0b8402
talhahassan850 <72450291+talhahassan850@users.noreply.github.com>
2020-12-17 13:53:55 +0500

    Update Connection.js

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//testing1
 const URI = "mongodb+srv://talhahassan:talhahassan@cluster0.1hpm8.mongodb.net/shirts?retryWrites=true&w=majority"
 
 const connectDB = async() => {
